c98509cf310a079d251767aad1d6f7850206bd60
KaiTheCoder <70276099+KaiTheCoder@users.noreply.github.com>
2022-12-09 17:34:15 +0600

    added all necessary files

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"butter-bot/bot"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	// token, ok := os.LookupEnv("BOT_TOKEN")
 
-	// if !ok {
-	// 	log.Fatal("Must set bot token in env: BOT_TOKEN")
-	// }
+	err := godotenv.Load("local.env")
+	
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	bot.Token = "MTA1MDcwMTM4NDY3ODM3NTQ1NA.GBDQoT._dH6XMF9PcAkZy-YIuuNUqsm_odNZVi-3RXZI0"
+	bot_token := os.Getenv("BOT_TOKEN")
+	bot.BotToken =  bot_token
 	bot.Run()
 }
