d05c68e846aeda3fd0ce2f9ec987af42f35d98ba
KaiTheCoder <70276099+KaiTheCoder@users.noreply.github.com>
2022-12-10 19:51:05 +0600

    removed extra code

--- main.go
+++ main.go
@@ -2,25 +2,8 @@ package main
 
 import (
 	"butter-bot/bot"
-	"log"
-	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-
-	err := godotenv.Load("local.env")
-	
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	botToken := os.Getenv("BOT_TOKEN")
-	guildID := os.Getenv("GUILD_ID")
-
-	bot.Token = botToken
-	bot.GuildID = guildID
-
 	bot.Run()
 }
